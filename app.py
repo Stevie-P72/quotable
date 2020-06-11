7635cb9db0a89d032ff9c59bfa844740175f078c
Stevie-P72 <stevier72@outlook.com>
2020-06-11 19:45:52 +0000

    connected to mongodb, added materialize framework

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 import os
+import pymongo
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'quotableDB'
-MONGODB_URI = os.getenv("QUOTABLEURI")
+app.config["MONGO_URI"] = os.getenv("QUOTABLEURI")
+mongo = PyMongo(app)
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Connected to Mongo")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Connection Failure: %S") % e
+
+
+@app.route('/')
+@app.route('/get_quotes')
+def get_quotes():
+    return render_template('timeline.html', quotes=mongo.db.Quotes.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
